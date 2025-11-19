dba63ee3d5158d501eb9d411c63d58fc1041cdf7
Kyuu <s3czsk@seznam.cz>
2025-11-19 13:20:03 +0100

    T29 SUper heavy experimental tank USA

---
+++ Zapiskos/19.11/roblox.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+console.warn("Hello, World!");
+console.table(["Hello, World!"]);
+console.error("Hello, World!");
+console.info("Hello, World!");
+
+let Maus = prompt("1 cislo");
+let Leopard2AV7 = prompt("2.Cislo");
+
+Maus = parseFloat(Maus);
+Leopard2AV7 = parseFloat(Leopard2AV7);
+
+let T30 = Maus + Leopard2AV7;
+
+console.log("Součet dvou čísel je: " + T30);
